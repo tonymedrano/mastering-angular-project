52ba5e661f515aeee0b65bb750334497f1b406c0
tonymedrano <tony.medrano.rodriquez@ing.com>
2019-08-15 12:33:34 +0200

    Adding adds components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { AdService } from "./ads/ad.service";
+import { AdItem } from "./ads/ad-item";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'Marvel Superheroes League NG';
+export class AppComponent implements OnInit {
+  title = "Marvel Superheroes League NG";
+  ads: AdItem[];
+
+  constructor(private adService: AdService){
+    
+  }
+
+  //. ::::: Checking lifecycle ::::: .\\
+
+  //. Called before ngOnInit() and whenever one or more data-bound input properties change.
+  ngOnChanges() {
+    this.log("ngOnChanges", 1);
+  }
+
+  //. Called once, after the first ngOnChanges().
+  ngOnInit() {
+    this.ads = this.adService.getAds();
+    this.log("ngOnInit", 2);
+  }
+
+  //. Called during every change detection run, immediately after ngOnChanges() and ngOnInit().
+  ngDoCheck() {
+    this.log("ngDoCheck", 3);
+  }
+
+  //. Called once after the first ngDoCheck().
+  ngAfterContentInit() {
+    this.log("ngAfterContentInit", 4);
+  }
+
+  //. Called after the ngAfterContentInit() and every subsequent ngDoCheck().
+  ngAfterContentChecked() {
+    this.log("ngAfterContentChecked", 5);
+  }
+
+  //. Called once after the first ngAfterContentChecked().
+  ngAfterViewInit() {
+    this.log("ngAfterViewInit", 6);
+  }
+
+  //. Called after the ngAfterViewInit() and every subsequent ngAfterContentChecked().
+  ngAfterViewChecked() {
+    this.log("ngAfterViewChecked", 7);
+  }
+
+  //. Called just before Angular destroys the directive/component.
+  ngOnDestroy() {
+    this.log("ngOnDestroy", 8);
+  }
+
+  //. Log the angular lyfecycle.
+  private log(lifecycle: string, order: number) {
+    console.log(
+      `%c ${order} - ${lifecycle}`,
+      "background: #222; color: #bada55; padding: 8px; font-size: 16px;"
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { HttpClientModule } from "@angular/common/http";
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDataService } from "./in-memory-data.service";
 
+//. Application
 import { AppComponent } from "./app.component";
 import { SuperheroesComponent } from "./superheroes/superheroes.component";
 import { SuperheroDetailComponent } from "./superhero-detail/superhero-detail.component";
 import { SuperheroMessagesComponent } from "./superhero-messages/superhero-messages.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { SuperheroDashboardComponent } from "./superhero-dashboard/superhero-dashboard.component";
-import { SuperheroSearchComponent } from './superhero-search/superhero-search.component';
+import { SuperheroSearchComponent } from "./superhero-search/superhero-search.component";
+
+//. Ads
+import { HeroJobAdComponent } from "./ads/hero-job-ad.component";
+import { AdBannerComponent } from "./ads/ad-banner.component";
+import { HeroProfileComponent } from "./ads/hero-profile.component";
+import { AdDirective } from "./ads/ad.directive";
+import { AdService } from "./ads/ad.service";
 
 @NgModule({
   declarations: [
@@ -21,7 +29,11 @@ import { SuperheroSearchComponent } from './superhero-search/superhero-search.co
     SuperheroDetailComponent,
     SuperheroMessagesComponent,
     SuperheroDashboardComponent,
-    SuperheroSearchComponent
+    SuperheroSearchComponent,
+    AdBannerComponent,
+    HeroJobAdComponent,
+    HeroProfileComponent,
+    AdDirective
   ],
   imports: [
     BrowserModule,
@@ -35,7 +47,8 @@ import { SuperheroSearchComponent } from './superhero-search/superhero-search.co
       dataEncapsulation: false
     })
   ],
-  providers: [],
+  providers: [AdService],
+  entryComponents: [ HeroJobAdComponent, HeroProfileComponent ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
